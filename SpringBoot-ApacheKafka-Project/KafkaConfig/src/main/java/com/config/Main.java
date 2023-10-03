a7e45d78785fb0c364af2786b7822d44d2ea77f2
Johan <jofiluren@outlook.com>
2023-10-03 12:33:14 +0200

    This endless complaining about main class not found fixed

---
+++ SpringBoot-ApacheKafka-Project/KafkaConfig/src/main/java/com/config/Main.java
@@ -0,0 +1,12 @@
+package com.config;
+
+/**
+ * Not a necessary class, but it's here to stop Maven from complaining about the lack of a main method
+ * when running mvn clean install.
+ * */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
